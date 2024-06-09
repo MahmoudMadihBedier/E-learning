478614a4ae30a2d5b701da892ed9436879dd6aa1
Mahmoud_Madih <moodbeder150@gmail.com>
2024-06-09 17:17:06 +0300

    E-learn features chat&pdf&videos

--- lib/core/Screens/signinScreen.dart
+++ lib/core/Screens/signinScreen.dart
@@ -1,15 +1,17 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:chatapp/core/Screens/chatScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+import 'package:lottie/lottie.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import '../../UIwidgets/buttonSINgUP.dart';
 import '../../learn/view/home_page.dart';
 
 class SigninScreen extends StatefulWidget {
-  static const String ScreenRoute='SigninScreen';
+  static const String ScreenRoute = 'SigninScreen';
+
   const SigninScreen({super.key});
 
   @override
@@ -17,107 +19,125 @@ class SigninScreen extends StatefulWidget {
 }
 
 class _SigninScreenState extends State<SigninScreen> {
-  bool LoadingIcon=false;
-  final _auth=FirebaseAuth.instance ;
-  static late final  user ;
+  bool LoadingIcon = false;
+  final _auth = FirebaseAuth.instance;
+
+  static late final user;
+
   late String Email;
-  late String Password ;
+  late String Password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: ModalProgressHUD(
         inAsyncCall: LoadingIcon,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                height: 180,
-                child: Image.asset("assets/Chatappicon.png"),
-              ),
-              SizedBox( height: 50,),
-              Padding(
-                padding: const EdgeInsets.all(25),
-                child: TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  textAlign: TextAlign.start,
-                  onChanged: (value){
-                    Email=value;
-                  },
-                  decoration: InputDecoration(
-                    hintText: ' Enter your E-mail ',
-                    contentPadding: EdgeInsets.symmetric(vertical: 10 ,horizontal: 20),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(30))
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green.shade300),
-                        borderRadius: BorderRadius.all(Radius.circular(30))
-                    ) ,
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xfffed064), width: 2),
-                        borderRadius: BorderRadius.all(Radius.circular(30))
-                    ) ,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: TextField(
-                  obscureText: true,
-                  textAlign: TextAlign.start,
-                  onChanged: (value){
-                    Password=value;
-                  },
-                  decoration: InputDecoration(
-                    hintText: ' Enter your password ',
-                    contentPadding: EdgeInsets.symmetric(vertical: 10 ,horizontal: 20),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(30))
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green.shade300),
-                        borderRadius: BorderRadius.all(Radius.circular(30))
-                    ) ,
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xfffed064), width: 2),
-                        borderRadius: BorderRadius.all(Radius.circular(30))
-                    ) ,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child: regsterButton(
-                  color: Color(0xFF96D6A4),
-                  title:" Sign in ",
-                  onprassed: ()async{
-                    setState(() {
-                      LoadingIcon=true;
-                    });
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final height = constraints.maxHeight;
+            final width = constraints.maxWidth;
+            const Duration duration = Duration(milliseconds: 800);
 
-                    try{
-                       user =await _auth.signInWithEmailAndPassword
-                        (email: Email, password: Password);
-                      if (user != null){
-                        Get.to(MyHomePage());
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    FadeInUp(
+                      duration: duration,
+                      delay: const Duration(milliseconds: 2000),
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 50),
+                        width: width,
+                        height: height * 0.4,
+                        child: Lottie.asset("assets/wl.json", animate: true),
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Padding(
+                      padding: const EdgeInsets.all(25),
+                      child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.start,
+                        onChanged: (value) {
+                          Email = value;
+                        },
+                        decoration: InputDecoration(
+                          hintText: ' Enter your E-mail ',
+                          contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.deepPurple),
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xfffed064), width: 2),
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: TextField(
+                        obscureText: true,
+                        textAlign: TextAlign.start,
+                        onChanged: (value) {
+                          Password = value;
+                        },
+                        decoration: InputDecoration(
+                          hintText: ' Enter your password ',
+                          contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.deepPurple),
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xfffed064), width: 2),
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: regsterButton(
+                        color:Colors.deepPurple,
+                        title: " Sign in ",
+                        onprassed: () async {
                           setState(() {
-                            LoadingIcon=false;
+                            LoadingIcon = true;
                           });
-                      }
-                    }catch(e){
-                      print(e);
-                    }
-                  },
+
+                          try {
+                            user = await _auth.signInWithEmailAndPassword(email: Email, password: Password);
+                            if (user != null) {
+                              Get.to(MyHomePage());
+                              setState(() {
+                                LoadingIcon = false;
+                              });
+                            }
+                          } catch (e) {
+                            print(e);
+                          }
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
-    );;
+    );
   }
 }
